40960a67b482dab3219ba00b43c3c019ba94e373
maestroabel <65257544+maestroabel@users.noreply.github.com>
2020-07-20 20:27:50 -0400

    Programas

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Programa_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1, B = 0;
+            double C = 0;
+
+            for (int i = 0; A > 0; i++)
+            {
+                try
+                {
+                    A = int.Parse(Console.ReadLine());
+                    B = B + A;
+                    C++;
+                }
+                catch (Exception)
+                {
+                    C = B / C;
+                    Console.WriteLine(C);
+                    break;
+                }
+            }
+        }
+    }
+}
